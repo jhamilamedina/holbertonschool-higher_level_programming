c2679da6a91d716967bb0efc3b696f4085275035
Maly Medina <jhamilamedina>
2023-09-19 23:36:33 -0700

    Python:Imprimir FizzBuzz(3,5)Fizz(3)Buzz(5)

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1, 101):
+        str = i
+
+        if (i % 3 == 0) & (i % 5 == 9):
+            str = 'FizzBuzz'
+        elif (i % 3 == 0):
+            str = 'Fizz'
+        elif (i % 5 == 0):
+            str = 'Buzz'
+        print("{0} ".format(str), end='')
