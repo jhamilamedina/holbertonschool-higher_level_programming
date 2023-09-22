ba9871705fed9153e36a2c0da1db5f6b6243cd6e
Maly Medina <jhamilamedina>
2023-09-21 23:20:04 -0700

    Python:imprime el número y la lista de sus argumentos

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == '__main__':
+    lenArgc = len(argv) - 1
+    strArgs = 'arguments:'
+
+    if lenArgc == 0:
+        strArgs = 'arguments.'
+    elif lenArgc == 1:
+        strArgs = 'arguments:'
+
+    print("{0} {1}".format(lenArgc, strArgs))
+
+    for i in range(1, lenArgc + 1):
+        print("{0}: {1}".format(i, argv[i]))
