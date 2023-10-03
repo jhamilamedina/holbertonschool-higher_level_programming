92a6eb7e5cbb458ab89b67730406402ce35025db
Maly Medina <jhamilamedina>
2023-10-02 17:41:05 -0700

    Python:Devolver una lista con los valores x4

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if roman_string not a string or None:
+        return 0
+    total = 0
+    prev = 0
+    for i in roman_string[::-1]:
+        valor 
