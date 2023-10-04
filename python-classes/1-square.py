b0d9b50f6033d41234df49b5faa34f53babf11e1
Maly Medina <jhamilamedina>
2023-10-03 22:51:14 -0700

    Python:Definir(square)clase y atributo privado(size)

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Definición de ua clase Square"""
+
+
+class Square:
+    """Representa la clase Square"""
+    def __init__(self, size):
+        """Inicializa el new Square
+        Args:
+            size(int): y es el tamaño de Square
+        """
+        self.__size = size
