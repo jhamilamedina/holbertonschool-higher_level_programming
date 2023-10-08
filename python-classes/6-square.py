eda927f5c4b576cc1ec1e4c58982816305d96b2f
Maly Medina <jhamilamedina>
2023-10-08 12:35:14 -0700

    Python:Imprimir 3 cdros 3x3(#)

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -10,8 +10,8 @@ class Square:
         Args:
             size(int): es el tamaño del square es igual a 0
         """
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
