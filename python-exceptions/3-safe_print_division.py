49700c1cf709659e7fc35e84a78f23e64c19e383
Maly Medina <jhamilamedina>
2023-10-09 18:48:34 -0700

    Python:Imprimir una división y el resultado

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    result = None
+    try:
+        result = a/b
+#Intenta finalizar la división
+    except ZeroDivisionError:
+        pass
+    finally:
+        print("Inside result: {0}".format(result))
+        return result
