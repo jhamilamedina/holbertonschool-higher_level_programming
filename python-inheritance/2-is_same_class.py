f9f49d052c5f1dbe895f5cf81248931564b44c5d
Maly Medina <jhamilamedina>
2023-10-19 21:38:05 -0700

    Python:Verificar si el objes instancia de una clase a_class

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Esta función verifica si el obj
+es una instancia de a_class
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Se define esta función que toma 2 argumentos
+    y verifica si el tipo de dato es concreto
+
+    Args:
+        obj (objeto): es el objeto que deseas verificar
+
+    Return:
+        Devuele True(booleano) si obj pertenece al
+        tipo de dato a_class, caso contrario False
+    """
+    return isinstance(obj, a_class)
