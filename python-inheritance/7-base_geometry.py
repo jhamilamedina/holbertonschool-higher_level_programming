62cb952dd4c63c6ab9eb1ca9564f2fb376f800ca
Maly Medina <jhamilamedina>
2023-10-20 21:30:12 -0700

    Python:Generar 3 mensajes de error TypeError, ValueError

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Se define una clase llamada BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """Representación de la clase"""
+    def area(self):  # Definimos un metodo area, argumento self
+        """
+        Esta función lanza una excepción
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Definimos un metodo y sus argmentos
+
+        Args:
+            value(int): valida el valor
+            name(str): nombre del parametro value
+
+        Return:
+            None
+        """
+        if type(value) != int:
+            raise TypeError("<name> must be an integer")
+        if value <= 0:
+            raise ValueError("<name> must be greater than 0")
