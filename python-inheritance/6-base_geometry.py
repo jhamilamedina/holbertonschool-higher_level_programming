531ff0666e8d51b73ebcadb632466ea6095dd4db
Maly Medina <jhamilamedina>
2023-10-20 21:06:11 -0700

    Python:Generar mensaje de excepcion:area() is not implemented

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Se define una clase llamada BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """Representación de la clase"""
+    def area(self):  # Definimos un metodo area, argumento self
+        """
+        Esta función lanza una excepción
+        """
+        raise Exception("area() is not implemented")
