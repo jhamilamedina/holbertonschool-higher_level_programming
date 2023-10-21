0b77fa1f127a81e85d9fd046dcd429ec1c793825
Maly Medina <jhamilamedina>
2023-10-20 21:33:17 -0700

    Python:Generar 3 mensajes de error TypeError, ValueError

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -24,6 +24,6 @@ class BaseGeometry:
             None
         """
         if type(value) != int:
-            raise TypeError("<name> must be an integer")
+            raise TypeError(f"{name} must be an integer")
         if value <= 0:
-            raise ValueError("<name> must be greater than 0")
+            raise ValueError(f"{name} must be greater than 0")
