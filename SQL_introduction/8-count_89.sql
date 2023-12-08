7bbca064472c64cc6ba5cbe11e60c499fc061e03
Maly Medina <jhamilamedina>
2023-12-07 17:24:15 -0800

    Add: Muestra los registros con id=89

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
 -- Muestra la cantidad de registros con id = 89 en la tabla first_table.
-SELECT COUNT(89) FROM first_table;
+SELECT COUNT(*) AS cantidad_registros_id_89 FROM first_table WHERE id = 89;
