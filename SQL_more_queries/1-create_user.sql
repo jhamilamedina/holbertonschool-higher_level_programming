f82740f447f65a5a07d6736bf62d64814565fe0c
Maly Medina <jhamilamedina>
2023-12-09 17:19:59 -0800

    Add: Crear user y password y tener todos los privilegios

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Crear el usuario y contraseña con todos sus privilegios.
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES on *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES
