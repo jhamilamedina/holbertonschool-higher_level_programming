78078efb594dd4289f340f7cf94d14151270b500
Maly Medina <jhamilamedina>
2023-12-15 15:11:16 -0800

    Add: Filtro por estados por entrada de usuario

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -10,26 +10,26 @@ import sys
 
 
 if __name__ == "__main__":
-     # Estableciendo la coneccion con MySQL
-     db = MySQLdb.connect(
+    # Estableciendo la coneccion con MySQL
+    db = MySQLdb.connect(
              host="localhost",
              user=sys.argv[1],
              passwd=sys.argv[2],
              port=3306,
              db=sys.argv[3],
     )
-     cu = db.cursor()
-     # Consulta para ordenar estados que empiecen con N ordenados por id
-     cu.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
-                .format(sys.argv[4]))
+    cu = db.cursor()
+    # Consulta para ordenar estados que empiecen con N ordenados por id
+    cu.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+               .format(sys.argv[4]))
 
-     # Recupera todas las filas(rows)
-     rows = cu.fetchall()
+    # Recupera todas las filas(rows)
+    rows = cu.fetchall()
 
-     # Mustra el resultado
-     for row in rows:
-         print(row)
-     
-     # Cerrar el cursor y la coneccion
-     cu.close()
-     db.close()
+    # Mustra el resultado
+    for row in rows:
+        print(row)
+
+    # Cerrar el cursor y la coneccion
+    cu.close()
+    db.close()
