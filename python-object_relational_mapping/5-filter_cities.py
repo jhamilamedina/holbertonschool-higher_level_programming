adeea52ae9bc52dce70cb0dbfefb869de3fef958
Maly Medina <jhamilamedina>
2023-12-15 16:19:14 -0800

    Inyección de SQL

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -12,23 +12,24 @@ import sys
 if __name__ == "__main__":
     # Estableciendo la coneccion con MySQL
     db = MySQLdb.connect(
-             host="localhost",
-             user=sys.argv[1],
-             passwd=sys.argv[2],
-             port=3306,
-             db=sys.argv[3],
+            host="localhost",
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            port=3306,
+            db=sys.argv[3],
     )
     cu = db.cursor()
     # Consulta para ordenar estados que empiecen con N ordenados por id
-    cu.execute("""SELECT * FROM states WHERE BINARY name = %s
-               ORDER BY id ASC"""
+    cu.execute("""SELECT cities.name FROM cities INNER JOIN states
+               ON states.id=cities.state_id
+               WHERE states.name=%S""". (sys.argv[4].))
 
     # Recupera todas las filas(rows)
     rows = cu.fetchall()
 
     # Mustra el resultado
-    for row in rows:
-        print(row)
+    tmp = list(row[0] for row in rows)
+    print(*tmp, sep=". ")
 
     # Cerrar el cursor y la coneccion
     cu.close()
