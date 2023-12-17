b45fe5f9219840edbacc685d8d427f6b78623cae
Maly Medina <jhamilamedina>
2023-12-17 12:59:58 -0800

    Test: Suma de nros enteros

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python3
+"""
+Este módulo contiene operaciones matemáticas
+
+Example:
+add_integer(4, 9)
+"""
+
+
 def add_integer(a, b=98):
-    """Verifica que a y b sean numeros enteros"""
-    if not isinstance(a, (int, float)):
-        raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
-        raise TypeError("b must be an integer")
+    """
+    Esta función suma dos enteros.
 
-    """Convertir a y b a numeros enteros si son flotantes"""
-    a = int(a)
-    b = int(b)
+    Args:
+        a (int): Primer entero
+        b (int): Segundo entero(valor por defecto 98)
 
-    """Calcula y devuelve la suma de a + b"""
-    return a + b
+    Returns:
+        int: return a + b
+    """
+    n = dict(a=a, b=b)
+
+    for k, v in n.items():
+        if not isinstance(v, (int, float)):
+            raise TypeError(f"{k} must be an integer")
+    return int(a) + int(b)
