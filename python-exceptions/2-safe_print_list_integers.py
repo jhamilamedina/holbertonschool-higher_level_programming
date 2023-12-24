1a08acf7ee38b64d26259674545eace47a5c04e6
Maly Medina <jhamilamedina>
2023-12-24 08:47:38 -0800

    Fix:Imprimir y contar números enteros

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -2,11 +2,13 @@
 def safe_print_list_integers(my_list=[], x=0):
     n_element = 0  # Inicialza el contador con los elementos impresos
 
-    for e in range(x):  # covierte el elemento a un entero
-        try:
-            print("{:d}".format(my_list[i]), end="")
-            n_element += 1
-        except (ValueError, TypeError):
-            continue
+    try:
+        for i in range(x):  # covierte el elemento a un entero
+            if isinstance(my_list[i], int):
+                print("{:d}".format(my_list[i]),
+                      end="\n" if i == x - 1 else " ")
+                n_element += 1
+    except (IndexError, TypeError):
+        pass
     print()  # Imprime una nueva linea
     return n_element  # Retora un numero real o entero
