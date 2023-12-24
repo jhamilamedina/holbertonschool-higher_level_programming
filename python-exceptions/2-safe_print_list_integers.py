db0ebd6ec8d09d36c9896483b0bf3467512dd7e8
Maly Medina <jhamilamedina>
2023-12-24 08:52:07 -0800

    Fix:Imprimir y contar números enteros

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -10,5 +10,6 @@ def safe_print_list_integers(my_list=[], x=0):
                 n_element += 1
     except (IndexError, TypeError):
         pass
-    
+    print(e)
+    print()
     return n_element  # Retora un numero real o entero
